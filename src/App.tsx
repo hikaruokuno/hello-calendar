5b3ea2dee3bb18b49adfcc2b3a480c1cf26b7df0
hikki <donot.say.lazy@gmail.com>
2021-06-01 15:53:07 +0900

    モックアップ作成 　・ヘッダー、イベント名のリスト

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,15 @@
 import React, { FC } from "react";
-import logo from "./logo.svg";
 import "./App.css";
+import Container from "@material-ui/core/Container";
+import Header from "./containers/common/Header";
+import EventNameList from "./containers/Home/EventNameList";
 
 const App: FC = () => (
   <div className="App">
-    <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-      <p>
-        Edit
-        <code>src/App.tsx</code>
-        and save to reload.
-      </p>
-      <a
-        className="App-link"
-        href="https://reactjs.org"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Learn React
-      </a>
-    </header>
+    <Container>
+      <Header />
+      <EventNameList />
+    </Container>
   </div>
 );
 
---
+++ src/containers/common/Header.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import IconButton from "@material-ui/core/IconButton";
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      flexGrow: 1,
+    },
+    menuButton: {
+      marginRight: theme.spacing(2),
+    },
+  })
+);
+
+const DenseAppBar: any = () => {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar variant="dense">
+          <IconButton
+            edge="start"
+            className={classes.menuButton}
+            color="inherit"
+            aria-label="menu"
+          />
+          <Typography variant="h6" color="inherit">
+            <strong>ハロカレ</strong>
+          </Typography>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+};
+
+export default DenseAppBar;
