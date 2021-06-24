d627f2f1f6cbc71a128a097e7f01b0c995377545
hikki <donot.say.lazy@gmail.com>
2021-06-25 00:11:18 +0900

    ヘッダーに検索ボタン追加

--- src/containers/common/Header.tsx
+++ src/containers/common/Header.tsx
@@ -1,17 +1,22 @@
 import React, { FC } from "react";
-import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
+import { createStyles, makeStyles } from "@material-ui/core/styles";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
 import IconButton from "@material-ui/core/IconButton";
+import SearchHeadForm from "components/Search/SearchHeadForm";
 
-const useStyles = makeStyles((theme: Theme) =>
+const useStyles = makeStyles(() =>
   createStyles({
     root: {
       flexGrow: 1,
     },
-    menuButton: {
-      marginRight: theme.spacing(2),
+    title: {
+      display: "block",
+    },
+    subTitle: {
+      flexGrow: 1,
+      display: "block",
     },
   })
 );
@@ -23,20 +28,26 @@ const DenseAppBar: FC = () => {
     <div className={classes.root}>
       <AppBar position="static">
         <Toolbar variant="dense">
-          <IconButton
-            edge="start"
-            className={classes.menuButton}
+          <IconButton edge="start" color="inherit" aria-label="menu" />
+          <Typography
+            className={classes.title}
+            variant="h5"
             color="inherit"
-            aria-label="menu"
-          />
-          <Typography variant="h5" color="inherit">
+            noWrap
+          >
             <strong>ハロカレ</strong>
           </Typography>
-          <Typography variant="subtitle2" color="inherit">
+          <Typography
+            className={classes.subTitle}
+            variant="subtitle2"
+            color="inherit"
+            noWrap
+          >
             &nbsp;&nbsp;&nbsp;&nbsp;ハロプロの
             <br />
             &nbsp;&nbsp;&nbsp;&nbsp;FCイベント情報サイト
           </Typography>
+          <SearchHeadForm />
         </Toolbar>
       </AppBar>
     </div>
