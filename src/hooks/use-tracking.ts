5a574bd6d12f95343e0ccbee8906ddc2e6a30950
hikki <donot.say.lazy@gmail.com>
2021-07-11 00:39:10 +0900

    analyticsへの送信に検索クエリを除外

--- src/hooks/use-tracking.ts
+++ src/hooks/use-tracking.ts
@@ -3,15 +3,16 @@ import { useLocation } from "react-router-dom";
 import googleAnalyticsConfig from "analytics-config";
 
 const useTracking = () => {
-  const { pathname, search } = useLocation();
+  // const { pathname, search } = useLocation();
+  const { pathname } = useLocation();
 
   useEffect(() => {
     if (!window.gtag) return;
     // eslint-disable-next-line @typescript-eslint/no-unsafe-call
     window.gtag("config", googleAnalyticsConfig.trackingId, {
-      page_path: `${pathname}${search}`,
+      page_path: pathname,
     });
-  }, [pathname, search]);
+  }, [pathname]);
 };
 
 export default useTracking;
