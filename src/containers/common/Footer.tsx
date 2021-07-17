225850d04c34a953e61a13c22ae9114442061c46
hikki <donot.say.lazy@gmail.com>
2021-07-17 20:44:23 +0900

    フッターのツイートボタン削除

--- src/containers/common/Footer.tsx
+++ src/containers/common/Footer.tsx
@@ -2,8 +2,6 @@ import React, { FC } from "react";
 import { createStyles, makeStyles } from "@material-ui/core/styles";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
-import { TwitterIcon, TwitterShareButton } from "react-share";
-// import Typography from '@material-ui/core/Typography';
 
 const useStyles = makeStyles(() =>
   createStyles({
@@ -27,26 +25,11 @@ const useStyles = makeStyles(() =>
 
 const Footer: FC = () => {
   const classes = useStyles();
-  const pushEventTracking = () => {
-    window.gtag("event", "tweet_click", {
-      event_category: "outbound",
-      event_label: "home",
-    });
-  };
 
   return (
     <div className={classes.root}>
       <AppBar position="static" color="default">
-        <Toolbar className={classes.toolBar}>
-          <TwitterShareButton
-            url="https://hellocale.com/"
-            title="イベントの予定をサクッと確認・登録  忙しいハロヲタのためのFCイベント情報サイト「ハロカレ」"
-            onClick={pushEventTracking}
-            className={classes.tweet}
-          >
-            <TwitterIcon size={30} round />
-          </TwitterShareButton>
-        </Toolbar>
+        <Toolbar className={classes.toolBar} />
       </AppBar>
     </div>
   );
