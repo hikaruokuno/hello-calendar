0d6047caf91245c47500fc095ee28bc689310164
hikki <donot.say.lazy@gmail.com>
2021-07-27 14:58:30 +0900

    mainとの競合を解消

--- src/containers/Search/SearchMain.tsx
+++ src/containers/Search/SearchMain.tsx
@@ -5,8 +5,6 @@ import ListCircular from "components/common/atoms/ListCircular";
 import { useLocation, useNavigate } from "react-router";
 import EventDetalsList from "components/common/list/EventDetalsList";
 import SearchForm from "components/Search/SearchForm";
-import Checkbox from "@material-ui/core/Checkbox";
-import FormControlLabel from "@material-ui/core/FormControlLabel";
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -28,26 +26,15 @@ const SearchMainContainer: FC = () => {
   const query = new URLSearchParams(useLocation().search).get("q");
   const param = query === null || query === undefined ? "" : query;
   const [values, setValues] = useState({ q: param });
-  const [past, setPast] = useState({
-    checked: false,
-  });
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const { performances, loading } = useEventDetailsSearch(
-    values.q,
-    {
-      limit: 30,
-    },
-    past.checked
-  );
+  const { performances, loading } = useEventDetailsSearch(values.q, {
+    limit: 30,
+  });
   const navigate = useNavigate();
 
   if (values.q !== param) {
     setValues({ q: param });
   }
-
-  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setPast({ ...past, [event.target.name]: event.target.checked });
-  };
 
   const handleChange2 = (
     targetName: string,
@@ -65,8 +52,6 @@ const SearchMainContainer: FC = () => {
     document.getElementById("search")!.focus();
   };
 
-  console.log("search-main");
-
   return (
     <>
       <SearchForm values={values} handleChange={handleChange2} clear={clear} />
@@ -76,20 +61,6 @@ const SearchMainContainer: FC = () => {
         </div>
       ) : (
         <>
-          <FormControlLabel
-            control={
-              <Checkbox
-                checked={past.checked}
-                onChange={handleChange}
-                name="checked"
-                color="primary"
-                size="small"
-              />
-            }
-            label="過去の公演を含む"
-            color="secondary"
-            className={classes.check}
-          />
           <EventDetalsList eventDetails={performances} />
         </>
       )}
