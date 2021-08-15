d243abf97d956d48d09f41e9c1c7d3a348f95cde
hikki <donot.say.lazy@gmail.com>
2021-08-15 11:08:17 +0900

    受付終了したけど、当落確認開始がまだのものを、当落開始日を表示するようにする 申込受付が本日のものをわかりやすいようにする

--- src/components/common/atoms/EventName.tsx
+++ src/components/common/atoms/EventName.tsx
@@ -3,6 +3,7 @@ import { makeStyles, createStyles } from "@material-ui/core/styles";
 import { Link } from "react-router-dom";
 import { ListItem, ListItemText, Typography } from "@material-ui/core";
 import { Event } from "services/hello-calendar/models/event";
+import { isToday, isTomorrow } from "date-fns";
 
 const useStyles = makeStyles(() =>
   createStyles({
@@ -31,18 +32,32 @@ const EventName: FC<{ event: Event }> = ({ event }) => {
   const isOpenConfirmPeriod =
     event.confirmStartDate!.toDate() <= new Date() && !event.isConfirmEnded;
   if (isOpenAppryPeriod) {
-    subTitle = " [受付中]";
+    if (isToday(event.applyEndDate!.toDate())) {
+      subTitle = " [受付中（本日締切）]";
+    } else {
+      subTitle = " [受付中]";
+    }
     color = "secondary";
     secondary = `申込期限: ${event.applyPeriodStr.substring(
       event.applyPeriodStr.indexOf("～") + 1
     )}`;
   } else if (isOpenConfirmPeriod) {
-    subTitle = " [当落確認期間中]";
+    if (event.paymentDate! && isTomorrow(event.paymentDate.toDate())) {
+      subTitle = " [当落確認期間中（本日支払期日）]";
+    } else {
+      subTitle = " [当落確認期間中]";
+    }
     color = "primary";
     secondary = `支払期限: ${event.paymentDateStr}`;
   } else {
     subTitle = " [受付終了]";
     color = "textSecondary";
+    if (!event.isConfirmEnded) {
+      secondary = `当落確認開始: ${event.confirmPeriodStr.substring(
+        0,
+        event.confirmPeriodStr.indexOf("～") + 1
+      )}`;
+    }
   }
 
   return (
